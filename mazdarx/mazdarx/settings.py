aa5ec9d6e6ffc70b405bd865a89bc9d7fadcaeeb
Breno-MT <hentgenp@gmail.com>
2023-07-27 18:05:18 -0300

    feat: added DriftConfig in INSTALLED_APPS

--- mazdarx/mazdarx/settings.py
+++ mazdarx/mazdarx/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drift.apps.DriftConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
