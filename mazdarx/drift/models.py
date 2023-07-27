3f8f9aa08a29a681736cf4be6d6bfd44a3dec459
Breno-MT <hentgenp@gmail.com>
2023-07-27 18:04:51 -0300

    feat: added Car and CarModel as new models for database

--- mazdarx/drift/models.py
+++ mazdarx/drift/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    car_name = models.CharField(max_length=70)
+    created_date = models.DateTimeField("Date announced")
+
+
+class CarModel(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    car_model = models.CharField(max_length=50)
