61deea2d87a908b4bb4bf6c260a8069c7f80e724
Breno-MT <hentgenp@gmail.com>
2023-07-27 17:53:02 -0300

    refact: changed TIME_ZONE to UTC-3 and add secret_key to .env

--- mazdarx/mazdarx/settings.py
+++ mazdarx/mazdarx/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+from dotenv import find_dotenv, load_dotenv
 
+load_dotenv(find_dotenv())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i2z5j2*&n-32(!cocuf8g!)^+z43i#m@*4v^$71zi1m_&sg0j$'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
