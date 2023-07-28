69eea69ef5b3cbfa2f7a5cd7cc050ff255a6ff5b
Breno-MT <hentgenp@gmail.com>
2023-07-28 17:40:22 -0300

    feat: added functions to Models

--- mazdarx/drift/models.py
+++ mazdarx/drift/models.py
@@ -1,11 +1,22 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Car(models.Model):
     car_name = models.CharField(max_length=70)
     created_date = models.DateTimeField("Date announced")
 
+    def was_created_recently(self):
+        return self.created_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.car_name
+
 
 class CarModel(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     car_model = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.car_model
